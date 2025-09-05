1aaec46661b6e098e6b194eabd7a46a1290567a1
sofiia <sofiia.khapal@nure.ua>
2025-09-05 15:01:23 +0200

    add logic according to strategy pattern

--- src/main/java/core/basesyntax/DiscountStrategy.java
+++ src/main/java/core/basesyntax/DiscountStrategy.java
@@ -8,14 +8,14 @@ import core.basesyntax.strategy.impl.NewYearDiscountService;
 
 public class DiscountStrategy {
     public DiscountService getDiscountServiceBySpecialEvent(String specialEvent) {
-        switch (specialEvent){
+        switch (specialEvent) {
             case "New Year": {
                 return new NewYearDiscountService();
             } case "Birthday": {
                 return new BirthdayDiscountService();
             }case "Black Friday": {
                 return new BlackFridayDiscountService();
-            } default:{
+            } default: {
                 return new DefaultDiscountService();
             }
         }
